613bf6bff880bb93de45434028045e74c7805036
Matheus Moizeis <matheusmoizeis@outlook.com>
2021-11-18 08:22:11 -0300

    feature(backend/service): Configuração inicial da camada service e configuracao para a entidade User.

--- backend/src/Manager.Domain/Entities/User.cs
+++ backend/src/Manager.Domain/Entities/User.cs
@@ -29,6 +29,8 @@ namespace Manager.Domain.Entities
             Email = email;
             Password = password;
             _errors = new List<string>();
+
+            Validate();
         }
 
         public User()
